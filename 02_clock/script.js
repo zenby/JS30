19eac9ede5235efae7eff59a1aa39fb7532e303f
Evgeniy Dainovich <zenbysel@gmail.com>
2018-09-10 00:25:38 +0300

    add clock

---
+++ 02_clock/script.js
@@ -0,0 +1,23 @@
+const arrowSecond = document.querySelector('.second');
+const arrowMinute = document.querySelector('.minute');
+const arrowHour = document.querySelector('.hour');
+
+setInterval(() => {
+  const now = new Date();
+  const secondArrowDeg = now.getSeconds() / 60 * 360 - 90;
+  arrowSecond.style.transform = `rotate(${secondArrowDeg}deg)`;
+
+  const minutesArrowDeg = now.getMinutes() / 60 * 360 - 90;
+  arrowMinute.style.transform = `rotate(${minutesArrowDeg}deg)`;
+
+  const hourArrowDeg = now.getHours() / 12 * 360 - 90;
+  arrowHour.style.transform = `rotate(${hourArrowDeg}deg)`;
+}, 1000);
+
+setTimeout(() => {
+  const arrows = document.querySelectorAll('.arrow');
+  const clock = document.querySelector('.clock');
+
+  clock.style.visibility = 'visible';
+  arrows.forEach(arrow => arrow.style.visibility = 'visible');
+}, 1000);
