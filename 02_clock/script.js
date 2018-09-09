85dd0eb6356deb93ed8507cb4fa3ba9446e93977
Evgeniy Dainovich <zenbysel@gmail.com>
2018-09-10 00:58:59 +0300

    remove unnecessary functions

--- 02_clock/script.js
+++ 02_clock/script.js
@@ -2,7 +2,10 @@ const arrowSecond = document.querySelector('.second');
 const arrowMinute = document.querySelector('.minute');
 const arrowHour = document.querySelector('.hour');
 
-setInterval(() => {
+calculateArrow();
+setInterval(calculateArrow, 1000);
+
+function calculateArrow() {
   const now = new Date();
   const secondArrowDeg = now.getSeconds() / 60 * 360 - 90;
   arrowSecond.style.transform = `rotate(${secondArrowDeg}deg)`;
@@ -12,12 +15,4 @@ setInterval(() => {
 
   const hourArrowDeg = now.getHours() / 12 * 360 - 90 + minutesArrowDeg / 12;
   arrowHour.style.transform = `rotate(${hourArrowDeg}deg)`;
-}, 1000);
-
-setTimeout(() => {
-  const arrows = document.querySelectorAll('.arrow');
-  const clock = document.querySelector('.clock');
-
-  clock.style.visibility = 'visible';
-  arrows.forEach(arrow => arrow.style.visibility = 'visible');
-}, 1000);
+}
